e2f42aa7297b26b289fe7a9b07a744f5e2fa8c83
Feuka <12001003397984@FEDIDCARD.GOV>
2024-08-22 12:02:51 -0600

    no NFSP covariate on occupancy, using seasonal random intercept and NCLD covariates

--- Functions/fit_zi_rem_occ_mod_binom_ea_area.R
+++ Functions/fit_zi_rem_occ_mod_binom_ea_area.R
@@ -30,26 +30,42 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
   require(sf)
   
   # data setup -------------------------------------------------
+  # study_site_grid$elim_area_idx[study_site_grid$Area_Name%in%c("0","1")] <- NA
+  
   elim_areas <- study_site_grid %>% 
     group_by(Area_Name) %>% 
     summarise(geometry=st_union(geometry)) %>% 
     mutate(area_m=st_area(.),
            area_km=area_m/1e6,
-           elim_area_idx= as.numeric(Area_Name)+1)
+           elim_area_idx=as.numeric(Area_Name)+1)
   
-  ssg_adj <- st_intersects(study_site_grid,remove_self=T)
-  ssg_adj <- as.carAdjacency(ssg_adj)
+  # ssg_adj <- st_intersects(study_site_grid,remove_self=T)
+  # ssg_adj <- as.carAdjacency(ssg_adj)
   
   sysbait_det_eff$Area_Name[is.na(sysbait_det_eff$Area_Name)] <- 0
-  sysbait_det_eff$elim_area_idx <- as.numeric(sysbait_det_eff$Area_Name)+1
-
+  # sysbait_det_eff <- sysbait_det_eff %>% select(-elim_area_idx)
+  
+  ## remove area outside of EAs and EA 1 ---------------
   rem_eff_ea$Area_Name[is.na(rem_eff_ea$Area_Name)]<- 0
+
+  rem_eff_ea <- rem_eff_ea %>% filter(Area_Name%in%c("4","6"))#filter(!(Area_Name%in%c("0","1"))) 
   rem_eff_ea$elim_area_idx <- as.numeric(rem_eff_ea$Area_Name)+1
+  # rem_eff_ea$elim_area_idx <- as.numeric(as.factor(rem_eff_ea$Area_Name))
+  
+  # sysbait_det_eff <- sysbait_det_eff %>% 
+  #   left_join(rem_eff_ea %>% ungroup() %>% 
+  #               select(elim_area_idx,Area_Name) %>% 
+  #               distinct())
+
+  # elim_areas <- elim_areas %>% filter(!(Area_Name%in%c("0","1"))) %>% 
+  #   left_join(rem_eff_ea %>% ungroup() %>% 
+  #               select(elim_area_idx,Area_Name) %>% 
+  #               distinct())
   
   rem_eff_ea <- rem_eff_ea %>% filter(!is.na(prop_ea_impact)) %>% 
     filter(!(method=="Ground" & is.na(eff_area_events))) %>% 
     filter(!(method%in%c("Aerial","Trap") & is.na(eff_area_hrs))) %>% 
-    filter(!(method=="Aerial" & eff_area_hrs>0.15)) %>% 
+    # filter(!(method=="Aerial" & eff_area_hrs>0.15)) %>% 
     rename(period_idx = period) %>% 
     group_by(period_idx,method,Area_Name) %>% 
     mutate(pass_idx=1:n()) 
@@ -59,12 +75,12 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
     filter(method=="Aerial") %>% 
     select(period_idx) %>% distinct
   a_pers_n <- which(!(1:max(rem_eff_ea$period_idx)%in% a_pers$period_idx))
-  eids <- unique(elim_areas$elim_area_idx)
+  eids <- sort(unique(rem_eff_ea$elim_area_idx))
   
   samp <- rem_eff_ea[1:(length(a_pers_n)*length(eids)),]
   samp$method <- "Aerial"
   samp$period_idx <- rep(a_pers_n,length(eids))
-  samp$elim_area_idx <- sort(rep(1:length(eids),length(a_pers_n)))
+  samp$elim_area_idx <- sort(rep(unique(eids),length(a_pers_n)))
   samp$Area_Name <-samp$Date <- NA
   samp$tot_rem <- samp$tot_hrs <- samp$num_events <- samp$eff_area_events <- samp$eff_area_hrs <-
     samp$prop_ea_impact <- samp$effect_area_km <- 0
@@ -91,12 +107,12 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
   
   dat_occ$trap_nights_km_sc <- scale(dat_occ$trap_nights_km)
 
-  elim_site_idx <- dat_occ %>% group_by(site_idx) %>% 
-     summarise(elim_area_idx=as.numeric(unique(elim_area_idx))) %>% 
+  elim_site_idx <- dat_occ %>% group_by(site_idx) %>%
+     summarise(elim_area_idx=as.numeric(unique(elim_area_idx))) %>%
      arrange(site_idx)
 
   ##covariates -----------------------------
-  nbeta <- 4
+  nbeta <- 3
   
   nlcd_siteid_orig <- nlcd_siteid
   nlcd_siteid$site_idx <- as.numeric(as.factor(nlcd_siteid$SiteID))
@@ -109,7 +125,7 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
   
   ##indices for nimble ---------------------------------
   nsites <- length(unique(dat_occ$site_idx))
-  nea <- length(unique(dat_occ$elim_area))
+  nea <- length(unique(dat_occ$elim_area_idx[!is.na(dat_occ$elim_area_idx)]))
   nperiods <- length(unique(dat_occ$period_idx))
 
   ## removal data-------------------
@@ -141,37 +157,37 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
   dat_aerial$eff_area_hrs_sc <- scale(dat_aerial$eff_area_hrs)
   dat_trap$eff_area_hrs_sc <- scale(dat_trap$eff_area_hrs)
   
+  nea_rem <- length(unique(rem_eff_ea$elim_area_idx))
+  ea_rem_idx <- data.frame(elim_idx_rem=1:nea_rem,
+                           elim_idx_orig=unique(rem_eff_ea$elim_area_idx))
+  
+  ## nfsp covariate ------------------
   remtot <- rem_eff_ea %>% group_by(elim_area_idx,period_idx) %>%
     reframe(removal=sum(tot_rem))
 
-  nfsp <- matrix(NA,nsites,nperiods)
-  for(i in 1:nsites){
-    for(t in 1:nperiods){
-      nfsp[i,t] <- unique(sysbait_det_eff$prp_nfs[as.numeric(sysbait_det_eff$site_idx)==i & sysbait_det_eff$period==t])
-    }
-  }
-  nfsp_sc <- scale(nfsp)
-  
+  # nfsp <- matrix(NA,nsites,nperiods)
+  # for(i in 1:nsites){
+  #   for(t in 1:nperiods){
+  #     nfsp[i,t] <- unique(sysbait_det_eff$prp_nfs[as.numeric(sysbait_det_eff$site_idx)==i & sysbait_det_eff$period==t])
+  #   }
+  # }
+  # nfsp_sc <- scale(nfsp)
+  # 
   ##removal matrix ------------------
   yrem <- matrix(0,nea,nperiods)
-  ea_idx <- sort(unique(rem_eff_ea$elim_area_idx))
+  ea_idx <- sort(unique(dat_occ$elim_area_idx))
   for(j in 1:nea){
     for(t in 1:nperiods){
       x <- remtot$removal[remtot$elim_area_idx==ea_idx[j] & remtot$period_idx==t]
       if(length(x)>0){
         yrem[j,t] <- x
-      }
-    }
-   }
+      }}}
 
   #one trap per mi2 -------------------------------
-  mn_te <- (1/2.59) * 
+  mn_te <- (1/2.59) /
       attr(dat_occ$trap_nights_km_sc,"scaled:scale") -
       attr(dat_occ$trap_nights_km_sc,"scaled:center")
-  # mn_te <- mean(dat_occ$trap_nights_km) * 
-  #   attr(dat_occ$trap_nights_km_sc,"scaled:scale") -
-  #   attr(dat_occ$trap_nights_km_sc,"scaled:center")
-
+  
   if(subset_data){
     samp_idx <- sample(unique(dat_occ$site_idx),20,replace=F)
 
@@ -189,6 +205,10 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
     agri <- agri[samp_idx,]
   }
   
+  # hist(rnbinom(10000,size=5,p=0.0001)/as.numeric(min(elim_areas$area_km)))
+  # summary(rbeta(100000,0.1,5))
+  # summary(rgamma(10000,6,1))
+  
   # fit model ---------------------------
   ## model specification -------------------------------------------------
   ZIbinomcode <- nimbleCode({
@@ -196,23 +216,25 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
     for(i in 1:nbeta){
       beta[i] ~ dlogis(0,1)
     }
+    sd_season ~ dgamma(10,10)
     
     for(i in 1:2){
       alpha[i] ~ dlogis(0,1)
       delta_t[i] ~ dlogis(0,1)
       delta_a[i] ~ dlogis(0,1)
     }
+    
     sd_pdet ~ dgamma(10,10)
     sd_theta_t ~ dgamma(10,10)
     sd_theta_a ~ dgamma(10,10)
-    
     sd_psi ~ dgamma(10,10)
     
-    p_n1 ~ dbeta(0.1,7)
-    r_n1 ~ dgamma(1,0.1)
-    mu_lam ~ dnorm(0,0.15)
-    sd_lam ~ dgamma(1,10)
+    p_n1 ~ dbeta(0.1,5)#dunif(0.0001,0.001)
+    r_n1 ~ dgamma(6,1) #dunif(1,5)
 
+    # lambda ~ dunif(0.7,1.3)
+    mu_lam ~ dnorm(0,0.15)
+    sd_lam ~ dgamma(1,20)
     # tau_sa ~ dgamma(10,10)
     
     #spatial autocorrelation
@@ -223,13 +245,31 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
     mu_pdet_mn <- alpha[1] + alpha[2]*mn_te
     logit(p_sys) ~ dnorm(mu_pdet_mn,sd=sd_pdet)
     
-    for(i in 1:nea){
-      log(lambda[i]) ~ dnorm(mu_lam,sd=sd_lam)
+    for(i in 1:nea_rem){
+      # lambda[elim_idx_rem[i]] ~ dunif(0.7,1.3)
+      log(lambda[elim_idx_rem[i]]) ~ dnorm(mu_lam,sd=sd_lam)
+      N[elim_idx_rem[i],1] ~ dnegbin(size=r_n1,prob=p_n1)
+      
+      for(t in 1:nperiods){
+        N_latent[elim_idx_rem[i],t] <- N[elim_idx_rem[i],t] * z_ea[elim_idx_rem[i],t]
+      }
+      
+      #abundace/removal process
+      for(t in 2:nperiods){
+        mu_nb[elim_idx_rem[i],t] <- lambda[elim_idx_rem[i]] * (N[elim_idx_rem[i],t-1] - yrem_mat[elim_idx_rem[i],t-1])
+        N[elim_idx_rem[i],t] ~ dpois(mu_nb[elim_idx_rem[i],t])
+        # N_change[i,t] <- (N[i,t]-N[i,t-1])/N[i,t-1]
+      }
     }
+    
+    for(t in 1:nperiods){
+      beta_season[t] ~ dnorm(beta[1],sd=sd_season)
+    }
+    
     for(k in 1:nsites){ #watershed loop
       for(t in 1:nperiods){
-        logit(psi[k,t]) <- beta[1] + beta[2]*nfsp[k,t] + 
-          beta[3]*develop[k] + beta[4]*agri[k] #+ sa[k]
+        logit(psi[k,t]) <- beta_season[t] + #beta[2]*nfsp[k,t] + 
+          beta[2]*develop[k] + beta[3]*agri[k] #+ sa[k]
         # logit(psi[k,t]) ~ dnorm(mu_psi[k,t],sd=sd_psi)
         
         #occupancy/detection
@@ -248,23 +288,12 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
         pelim[k,t] <- pabs[k,t]/(pabs[k,t] + psi[k,t]*(1-pstar_site[k,t]))
       }
     }
-
-    for(i in 1:nea){ #elimination area loop
-      #initalize N
-      N[i,1] ~ dnegbin(size=r_n1,prob=p_n1)
-      
-      #hierarchical z
+    
+    #estimate all EA occupancy
+    for(i in 1:nea){
       psi_ea[i] ~ dbeta(1,1)
       for(t in 1:nperiods){
         z_ea[i,t] ~ dbern(psi_ea[i])
-        N_latent[i,t] <- N[i,t] * z_ea[i,t]
-      }
-      
-      #abundace/removal process
-      for(t in 2:nperiods){
-        mu_nb[i,t] <- lambda[i] * (N[i,t-1] - yrem_mat[i,t-1])
-        N[i,t] ~ dpois(mu_nb[i,t])
-        # N_change[i,t] <- (N[i,t]-N[i,t-1])/N[i,t-1]
       }
     }
     
@@ -334,7 +363,7 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
   ### constants -----------------------------
   const <- list(develop=develop$develop_sc,
                 agri=agri$agri_sc,
-                nfsp=nfsp_sc,
+                # nfsp=nfsp_sc,
                 nbeta=nbeta,
                 nsites=nsites,
                 nperiods=nperiods,
@@ -342,7 +371,9 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
                 site_idx_occ=dat_occ$site_idx,
                 period_idx_occ=dat_occ$period_idx,
                 ea_site_idx=elim_site_idx$elim_area_idx,
+                elim_idx_rem=ea_rem_idx$elim_idx_orig,
                 nea=nea,
+                nea_rem=nea_rem,
                 elim_prob=elim_prob,
                 mn_te=mn_te,
                 eff_weeks=eff_weeks,
@@ -362,16 +393,17 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
 
   ### initial values -----------------------------
   inits <- list(beta=rnorm(nbeta,c(0,0.5),0.1),
-                mu_lam=rnorm(1,0,0.1),
-                sd_lam=rnorm(1,0.1,0.01),
+                # lambda=rnorm(1,1,0.05),
+                mu_lam=rnorm(1,0,0.05),
+                sd_lam=rnorm(1,0.01,0.005),
                 N = matrix(NA,nea,nperiods),
                 z_site = matrix(1,nsites,nperiods),
-                p_n1 = rnorm(1,0.001,0.0001),
-                r_n1 = rpois(1,7),
+                p_n1 = rnorm(1,0.0001,0.00001),
+                r_n1 = rnorm(1,6,0.1),
                 z_ea = matrix(1,nea,nperiods),
                 psi_ea = rnorm(nea,0.95,0.01)
   )
-  inits$N[,1] <- apply(yrem,1,max)*25
+  inits$N[,1] <- apply(yrem,1,max)*10
 
   ## nimble configuration -------------------------------------------------
   mod <- nimbleModel(code = ZIbinomcode,
@@ -385,6 +417,7 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
   # if(is.na(monitors)){
   monitors <- c("lambda",
                 "beta",
+                "beta_season",
                 "alpha",
                 "sd_pdet",
                 "delta_t",
@@ -402,6 +435,8 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
                 "N_latent",
                 "pip_a",
                 "pip_t",
+                "mu_lam",
+                "sd_lam",
                 "yrem_pred_aerial",
                 "yrem_pred_trap",
                 "yocc_pred")
@@ -419,12 +454,13 @@ fit_zi_rem_occ <- function(sysbait_det_eff, #output of data_functions_ws_occ_ea_
                       # setSeed = 1:nChains,
                       nchains = nChains)
   
+  # Cmod$mod$N
   # Cmod$mod$lambda
   # Cmod$mod$calculate("cpue")
   # Cmod$mod$calculate("r_n1")
   # Cmod$mod$calculate("p_n1")
 
-  # niter<-100000
+  # niter<-50000
   # burnProp<-0.75
   # thin <-5
 
